d78b75feb3cbe2f246f8f0fbeeda661fc931093c
Rico Rojas <djizco@yahoo.com>
2016-06-29 17:21:39 -0500

    Step 2 Complete

--- app/index.js
+++ app/index.js
@@ -1,18 +1,10 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-// var routes = require('./config/routes.js');
+var routes = require('./config/routes.js');
 
-var HelloWorld = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-})
+
 
 ReactDom.render(
-  <HelloWorld/>,
+  routes,
   document.getElementById('app')
 );
